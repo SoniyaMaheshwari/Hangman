768d3a5621ad81dfa1faceb3e8d3badf2ed2359a
SoniyaMaheshwari <sagar@Sagars-MacBook-Air.local>
2023-07-20 15:54:25 +0100

    Project completion

--- README.md
+++ README.md
@@ -2,17 +2,20 @@
 Hangman is a guessing game for two or more players. One player thinks of a word,  and the other(s) tries to guess it by suggesting letters or numbers within a certain number of guesses.
 In this project, computer randomly selects a word and a player tries to guess it.
 
-## Milestone1
-Here, I have used python language to implement this game.
+## Milestone 1 
+Github cloning the repository
 
 ## Milestone2
+Here, I have used python language to implement this game.
+
+## Milestone3
 
 Here I have created two functions.
 1)ask_for_input() function: It takes a single alphabet as input from the user
 2)check_guess() function: It checks if the letter is in the word.
 User is given 5 attempts to guess the correct word.
 
-## Milestone3
+## Milestone4
 
 Here I have created a class called Hangman
 a) It has following attributes
@@ -26,3 +29,7 @@ b) It has following methods
 * check_guess() method
 * maskWord() method: It is used to unmask the letters from the word as user guesses them.
 
+## Milestone5
+Here I have put all the logic(class, methods) in a function called play_game().
+I have passed word_list as argument to this function
+Outside the function, I have called play_game function to play the game. 
---
+++ milestone_5.py
@@ -0,0 +1,94 @@
+
+from curses.ascii import isalpha
+import random
+import string
+
+def play_game(word_list):
+    num_lives = 3
+    print("You have 5 lives")
+    #player1 = Hangman(word_list,num_lives)
+   
+        
+
+
+    class Hangman: 
+        def __init__(self,word_list, num_lives):
+            self.word_list = word_list
+            self.num_lives = num_lives
+
+            self.word = random.choice(self.word_list) # this is to randomly choose a letter from word_list
+            self.word1 = self.word
+            self.word_guessed = []
+            self.num_letters = list(string.ascii_uppercase) # this contains the list of all letters
+            self.list_of_guesses = []
+            self.state = "_" * len(self.word) # mask the word
+            self.ask_for_input()
+           
+        
+
+        #This method asks for input from the user and checks if the input is a single letter and a alphabet
+        def ask_for_input(self):
+            
+            while True:
+                if self.num_lives: # check if usser has exhausted all his attempts
+                    #print("*******************************************************")
+                    print(f"\n\n{self.state}\n")
+                    print(f"Guess letters from \t {self.num_letters}")
+                    self.guess= input("\nEnter a single letter \t ").lower()
+                    
+                    #self.guess=self.guess.lower()
+                    if (len(self.guess) == 1 and isalpha(self.guess)):
+                        if self.guess in self.list_of_guesses:
+                            print("\n You already guessed this letter")
+                            continue
+                        else:
+                            
+                            self.list_of_guesses.append(self.guess)
+                            self.num_letters.remove(self.guess.upper())
+                            self.check_guess()
+                            
+                    else:
+                        print("\n Oops! That is not a valid input.") # This statement is executed when the user enter incorrect input
+                        continue
+                else:
+                    print("\n Sorry!, You Lost\n")
+                    break #want to know why break statement is not working here, Moderator please explain.
+                return
+                
+                
+
+        #This method checks if the user has correctly guessed the letter             
+        def check_guess(self):
+            if(self.guess in self.word): 
+                print(f"\nGood Guess! \t {self.guess} is in word")
+                self.word = self.word.translate({ord(self.guess):None})
+                self.state =self.maskWord(self.state,self.word1,self.guess)
+                
+                
+                if(len(self.word) == 0):
+                    print(f"\n Congratulations !!! You won the game! {self.word1.upper()} is the word\n")
+                    return
+                    
+                self.ask_for_input()
+
+            else:
+                print(f"\n Sorry, {self.guess} is not in the word. Try again." ) 
+                self.num_lives -= 1 
+                print(f"\n You have {self.num_lives} lives left")
+                self.ask_for_input()
+
+
+    #This method is to mask and unmask the letters of the word to be guessed
+        def maskWord(self,state, word, guess):
+            state = list(state)
+            for i in range(len(word)):
+                if word[i] == guess:
+                    state[i] = guess.upper()
+            return "".join(state)
+
+    player1 = Hangman(word_list,num_lives)
+   
+    return
+
+word_list = ["apple", "orange",'strawberry', 'peach', 'plum']
+play_game(word_list)
